4110cb39e25ed5147f8f6874a36d5ab7a6de1686
Mark Carlson <mark.carlson@takeoff.com>
2019-07-20 13:03:07 -0700

    Adds old school stateful component demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log('btn clicked!');
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Click counter: {this.state.counter}</h1>
+        <button onClick={this.handleClick}>Click me!</button>
+      </div>
+    );
+
+  }
 }
 
 export default App;
